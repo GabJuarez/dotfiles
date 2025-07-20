9825ca47dabcaf821bab137a4844da3197829a85
GabJuarez <gabjuarez@uamv.edu.ni>
2025-07-20 15:46:33 -0600

    Agregando configuracion base

---
+++ nvim/init.lua
@@ -0,0 +1,70 @@
+-- Cargar packer
+vim.cmd [[packadd packer.nvim]]
+
+-- Cargar plugins
+require('plugins')
+
+-- Opciones básicas
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.showcmd = true
+vim.opt.encoding = 'utf-8'
+vim.opt.showmatch = true
+
+-- indent line (indent-blankline.nvim)
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↴")
+require("ibl").setup()
+
+-- Iconos para nvim-tree y otros
+require('nvim-web-devicons').setup()
+
+
+-- LSP para C y Python
+local lspconfig = require('lspconfig')
+lspconfig.clangd.setup{}
+lspconfig.pyright.setup{}
+
+-- Autocompletado con nvim-cmp y luasnip
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>']     = cmp.mapping.abort(),
+    ['<CR>']      = cmp.mapping.confirm({ select = true }),
+    ['<C-j>']     = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ['<C-k>']     = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+  })
+})
+
+-- Atajos personalizados
+vim.keymap.set("n", "<C-d>", ":CocCommand prettier.formatFile<CR>", { silent = true }) -- Formateo
+vim.keymap.set("n", "<C-f>", ":NvimTreeToggle<CR>", { noremap = true, silent = true }) -- Explorador
+
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,115 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim' -- como gestor de plugins ocupo packer
+
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+
+  -- autocompletado
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+
+  --indent line
+  use("lukas-reineke/indent-blankline.nvim")
+
+  -- plugin prettier
+  use {'neoclide/coc.nvim', branch = 'release'}
+
+  -- Snippets
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+
+  use({
+  'projekt0n/github-nvim-theme',
+  config = function()
+    require('github-theme').setup({
+      
+    })
+
+    vim.cmd('colorscheme github_dark_default')
+  end
+  })
+
+  -- gestor de archivos
+ use {
+  'nvim-tree/nvim-tree.lua',
+  requires = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require("nvim-tree").setup({
+      renderer = {
+        icons = {
+          show = {
+            git = true,         -- mostrar iconos de git
+            folder = true,      -- mostrar iconos de carpetas
+            file = true,        -- mostrar iconos de archivos
+            folder_arrow = true -- mostrar flechas de carpeta
+          },
+        },
+      },
+    })
+  end
+}
+
+  -- barra de estado
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'gruvbox-material',
+          icons_enabled = false,
+          section_separators = '',
+          component_separators = ''
+        }
+      }
+    end
+  }
+
+  -- barra de pestañas para buffers (tabs)
+  use {
+    'akinsho/bufferline.nvim',
+    requires = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require("bufferline").setup {
+        options = {
+          diagnostics = "nvim_lsp",
+          show_buffer_icons = false, -- quita íconos si quieres
+          show_buffer_close_icons = true,
+          separator_style = "thin",
+          always_show_bufferline = true,
+          offsets = {
+            {
+              filetype = "NvimTree",
+              text = "Explorador",
+              highlight = "Directory",
+              text_align = "left",
+            }
+          },
+        }
+      }
+    end,
+  }
+
+   -- panel de errores parecido a errror lens
+  use {
+    "folke/trouble.nvim",
+    requires = "nvim-tree/nvim-web-devicons", -- para los iconos
+    config = function()
+      require("trouble").setup {
+        auto_open = false,
+        auto_close = true,
+        use_lsp_diagnostic_signs = true,
+      }
+    end
+  }
+
+
+end)
+
